65b1e9aa698a9fa484a98ffb97bfd3263990fe3d
isaaclauture10 <isaaclauture10@gmail.com>
2026-06-03 17:35:36 -0400

    Update Main.py
    Day 4 - Added task prioritization and summary

--- Main.py
+++ Main.py
@@ -5,8 +5,22 @@ tasks = [
     {"name": "BUSA Quiz", "status": "In Progress"},
     {"name": "Resume Update", "status": "Missed"}
 ]
+high_priority_count = 0
 
 print("\nTODAY'S TASKS:")
 
 for task in tasks:
-    print("-", task["name"], "|", task["status"])
+
+    if task["status"] == "Not Started":
+        priority = "HIGH PRIORITY"
+        high_priority_count += 1
+
+    elif task["status"] == "In Progress":
+        priority = "MEDIUM PRIORITY"
+
+    else:
+        priority = "RECOVERY PLAN"
+
+    print("-", task["name"], "|", priority)
+
+print(f"\nYou have {high_priority_count} high-priority tasks to complete today.")
